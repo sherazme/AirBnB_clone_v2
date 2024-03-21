48c2f52a58d8d3fd6b7ba090902fa6b59478dd68
sherazme <sheraz.m.e@gmail.com>
2024-03-20 19:29:53 -0700

    mysql

--- models/state.py
+++ models/state.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import models
-from os import getenv
-from models.base_model import Base
 from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    if models.is_type == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if models.is_type != 'db':
         @property
         def cities(self):
-            """Get list of related Cities"""
-            c_l = []
-            for city in list(models.storage.all(City).values()):
+            cities_list = []
+            all_cities = models.storage.all(City).values()
+            for city in all_cities:
                 if city.state_id == self.id:
-                    c_l.append(city)
-            return c_l
+                    cities_list.append(city)
+            return cities_list
